205d23ce94dc098ab2e368e90d4362b7830ba3fd
Code-Omega <Clue@citeswpaurhprv-172-16-47-13.near.illinois.edu>
2015-02-07 15:56:10 -0600

    add files for Mp1

---
+++ New Unity Project/Assets/LightScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+		//var lt = GetComponent.<Light>();
+		//lt.color = Color.blue;
+}
+
+function Update () {
+		var lt = GetComponent.<Light>();
+		//lt.color = Color.blue;
+		if (Input.GetKeyDown (KeyCode.Tab)){
+			print ("tab key was pressed");
+			lt.enabled = !lt.enabled;
+		}
+		var player = gameObject.Find("FPC1");
+		var dist = Vector3.Distance(player.transform.position, Vector3(0,0,0));
+		lt.intensity = 7-dist;
+		//print(dist);
+}
---
+++ New Unity Project/Assets/roomColor.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+	for (var child : Transform in transform) {
+		child.renderer.material.color = Color(1,0,0);
+	}
+}
+
+function Update () {
+}
